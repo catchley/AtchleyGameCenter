95fd8e36ba9412bdd775d286b081da384532cfb8
Chris Atchley <steve.chris.atchley@gmail.com>
2016-11-09 19:27:04 -0500

    Added "sex" property to Player class.

--- AtchleyGameCenter/Models/Player.cs
+++ AtchleyGameCenter/Models/Player.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AtchleyGameCenter.Models
 {
+    public enum Sex
+    {
+        Male, Female
+    }
     public class Player
     {
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        public Sex Sex { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
         public DateTime Birthday { get; set; }
 
         public virtual ICollection<Signup> Signups { get; set; }
